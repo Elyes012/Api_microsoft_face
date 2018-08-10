c621197d2f9b536b42cd5e33590fa5f57d29d3a9
Elyes012 <maamouri.ilyes@gmail.com>
2018-08-10 17:12:47 +0200

    add readme

---
+++ Readme.md
@@ -0,0 +1,17 @@
+Titre: Reconnaissance faciale
+
+Description: 
+Lors du démarrage de la page "index.html", l'utilisateur trouvera le bouton "AnalyseFace qui ouvrir sa caméra vidéo(accessible par un micro-ordinateur, téléphone intelligent, ...)aprés avoir autoriser l'utilisation de sa caméra
+et prendre une photo qui s'affichera sur l'écran ensuite l'utilisateur peut avoir une description de son visage(age, couleur des cheveux, couleur des yeux, émotions...) selon sa photo déja prise
+
+Les outils utilisés :
+    - Serveur Web : hébergment local ou hébergement web en ligne
+    - Editeur: Microsoft visuel studio code
+    - Languages: HTML5, Javascript, Css
+    - FrameWork: Jquery
+    - API: api.cognitive.microsoft.com
+    
+Aperçu sur le site web :
+    Lien github : https://github.com/Elyes012/myfaceid
+    Lien github page : https://elyes012.github.io/myfaceid/faceid.html
+   
--- js/script.js
+++ js/script.js
@@ -3,17 +3,31 @@
 
 const YOUR_KEY = "42d7ffc536a7429682084973de780720";
 function sendToMS(callback) {
+ 
     canvas.toBlob(function (blob) {
         var xhr = new XMLHttpRequest();
-        
+         
         xhr.onload = f=> callback(xhr.response);
-        
-        xhr.open('POST', 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=true&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise');
+      
+       
+     xhr.open('POST', 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=true&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise');
+     
+   
+    
         xhr.setRequestHeader("Content-Type", "application/octet-stream");
         xhr.setRequestHeader("Ocp-Apim-Subscription-Key", YOUR_KEY);
         xhr.send(blob);
-    }, 'image/jpeg', .8);
+     
+        console.log(xhr);
+        
+  
+    });
+
+  
+ 
 }
+
+
 
 navigator.mediaDevices.getUserMedia({
         video: true
@@ -26,5 +40,9 @@ navigator.mediaDevices.getUserMedia({
         canvas.height = vid.videoHeight;
         canvas.getContext('2d').drawImage(vid, 0, 0);
         sendToMS(text => log.textContent = text);
+      
     })
   }
+ function newobjet() {
+  
+ }
